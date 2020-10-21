182d73765a58ab4a43e9ef28186d3baf8838c36f
dsctut <dsctut@gmail.com>
2020-10-21 17:26:20 +0200

    Navigation Drawer Layout

--- app/src/main/java/com/example/irecipe/MainActivity.java
+++ app/src/main/java/com/example/irecipe/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.irecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView TestNavDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Test nav drawer layout
+        TestNavDrawer = (TextView)findViewById(R.id.testNavLayout);
+        TestNavDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent navDrawer = new Intent(MainActivity.this, NavigationDrawer.class);
+                startActivity(navDrawer);
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/example/irecipe/NavigationDrawer.java
+++ app/src/main/java/com/example/irecipe/NavigationDrawer.java
@@ -45,6 +45,9 @@ public class NavigationDrawer extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Insert code for displaying user picture, name and email on the header view
+
     }
 
     @Override
